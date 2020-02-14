a2936c6b8e54f1828c957135b6e6d7f8c6e51cb7
helloworld-LCL <56032185+helloworld-LCL@users.noreply.github.com>
2020-02-14 17:50:52 +0100

    Add files via upload

--- run_auctions.py
+++ run_auctions.py
@@ -46,10 +46,10 @@ s = 10          #sellers
 n = b+s         #number of households
 
 #define parameters for all distribution functions
-#param = [mean, standard dev, lower bound, upper bound]
-
 FiT = 0.08      #euro/kWh Feed in Tariff
 ToU = 0.5       #euro/kWh (peak rate) Time of Use
+
+#param =    [mean, standard dev, lower bound, upper bound] (for now these are all for truncated uniform distributions)
 
 #buyers
 param_b_v = [0.4,   0.1,  0,      ToU]
@@ -60,15 +60,17 @@ param_s_v = [0.2,   1,    FiT,    1]  #the 1 euro/kWh is for upper bound only, f
 param_s_q = [0.8,   1,    0.2,    2]
 
 'create necessary dataframes'
-MCP_df = df_MCP()
-eff_df = df_allo_eff()
-quantity_traded_df = df_quantity_traded()
+#dataframes used for each iteration
+MCP_df = df_MCP()                               #dataframe (df) for saving the MCP
+utility_profits_df = df_utility_profits()       #create df that keep tracks of Buyers and Sellers utilty and profits over iterations
+quantity_traded_df = df_quantity_traded()       #create df that keep tracks of quantity traded
+eff_df = df_allo_eff()                          #dataframe for calculations for Huang mechanism allocative efficiency (2/14 not important for now)
 
-MCP_df_mean = df_MCP()
-eff_df_mean = df_allo_eff()
+#dataframes used for analysis over all iterations
+MCP_df_mean = df_MCP()                          #dataframe for saving the average MCP's over all iterations run
+eff_df_mean = df_allo_eff()                     #dataframe for saving the average efficiency over all iterations run (useful only when comparing different number of agents)
 
-#create df that keep tracks of Buyers and Sellers utilty and profits over iterations
-utility_profits_df = df_utility_profits()
+'set iterations'
 total_iterations = 100
 
 for index in range(total_iterations):
@@ -79,35 +81,28 @@ for index in range(total_iterations):
     s_v = create_sellers_v_truncnorm(s, param_s_v)
     s_q = create_sellers_q_truncnorm(s, param_s_q)
 
+    #create buyer and seller dataframes where prices and allocations of each mechanism result will be saved
     buyers_df = df_buyers(b_v, b_q)
     sellers_df = df_sellers(s_v, s_q)
 
-    '''check if there are sellers and buyers, if not there are no agents for trade and everyone'''
-    if len(buyers_df) == 0:
-        print('there are no buyers')
-        eff_df.loc[index] = 0
-        MCP_df.loc[index] = 0
-
-    elif len(sellers_df) == 0:
-        print('there are no sellers')
-        eff_df.loc[index] = 0
-        MCP_df.loc[index] = 0
+    '''start of Average Auction (this is the default Social Welfare maximizing mechanism acting as a benchmark)'''
+    # run auction and get result from SW maximization
+    buyers_allocation, sellers_allocation, result = run_normal_double_auction(buyers_df, sellers_df)
+    # updates new column to buyers and sellers df with the social welfare maximizing allocations
+    buyers_df, sellers_df = update_deafult_mechanism(buyers_allocation, sellers_allocation,
+                                                     buyers_df, sellers_df)
+    #check if any matches are made
+    if sum(buyers_df['Average allocation'])==0:
+        continue
 
     else:
-        '''start of Average Auction'''
-        # run auction and get result from SW maximization
-        buyers_allocation, sellers_allocation, result = run_normal_double_auction(buyers_df, sellers_df)
-
-        # updates new column to buyers and sellers df with the social welfare maximizing allocations
-        buyers_df, sellers_df = update_deafult_mechanism(buyers_allocation, sellers_allocation,
-                                                         buyers_df,         sellers_df)
         # find buyers and sellers in merit order, get the MCP of the auction
         buyers_in_merit_default, sellers_in_merit_default, MCP_default = get_average_mechanism_MCP(buyers_df,
                                                                                                    sellers_df)
         # update MCP of Average auction to dfs
         buyers_df, sellers_df = update_average_MCP_to_df(buyers_allocation, sellers_allocation,
-                                                             buyers_df,         sellers_df,
-                                                             MCP_default)
+                                                         buyers_df, sellers_df,
+                                                         MCP_default)
 
         '''start of VCG'''
         # VCG: Clarke Pivcot Rule
@@ -116,49 +111,54 @@ for index in range(total_iterations):
         MCP_buyers_VCG, MCP_sellers_VCG = get_VCG_mechanism_MCP(VCG_p_buyers, VCG_p_sellers)
 
         # VCG: update prices and allocations to dataframes
-        buyers_df, sellers_df = update_VCG_prices_and_allocation(VCG_p_buyers,  VCG_p_sellers,
-                                                                 buyers_df,     sellers_df)
-        # buyers pay less than what sellers receive
+        buyers_df, sellers_df = update_VCG_prices_and_allocation(VCG_p_buyers, VCG_p_sellers,
+                                                                 buyers_df, sellers_df)
 
         '''start of Huang'''
         # Huang: market clearing price rule
-        buyers_in_merit_Huang, sellers_in_merit_Huang, MCP_buyers_Huang,  MCP_sellers_Huang = get_Huang_mechanism_MCP(
-                                                                                    buyers_df,
-                                                                                    sellers_df)  # result of this is what we need
+        buyers_in_merit_Huang, sellers_in_merit_Huang, MCP_buyers_Huang, MCP_sellers_Huang = get_Huang_mechanism_MCP(
+            buyers_df,
+            sellers_df)  # result of this is what we need
         # Huang: allocation rule
         q_vector, d_vector = allocation_Huang_mechanism_Q_a(buyers_in_merit_Huang, sellers_in_merit_Huang)
 
         # Huang: prices and allocations to dataframe
-        buyers_df, sellers_df = update_Huang_to_df(q_vector,            d_vector,
-                                                   buyers_df,           sellers_df,
-                                                   MCP_buyers_Huang,    MCP_sellers_Huang)
+        buyers_df, sellers_df = update_Huang_to_df(q_vector, d_vector,
+                                                   buyers_df, sellers_df,
+                                                   MCP_buyers_Huang, MCP_sellers_Huang)
 
-        # save results of this iteration in df by index
-        MCP_df.loc[index] = [MCP_default, MCP_buyers_VCG, MCP_sellers_VCG, MCP_buyers_Huang, MCP_sellers_Huang]
+        '''save results of iteration to dataframes'''
+        # update MCP of this iteration
+        MCP_vector_iteration = [MCP_default, MCP_buyers_VCG, MCP_sellers_VCG, MCP_buyers_Huang, MCP_sellers_Huang]
 
+        MCP_df = update_iteration_MCP(MCP_vector_iteration, MCP_df)
+
+        # find and save profits and utilities of each mechanism for this iteration
+        utility_profits_df = calculate_profits_utility(buyers_df, sellers_df, utility_profits_df)
+
+        # find quantity traded for each mechanism for this iteration
+        quantity_traded_df = calculate_market_liquidity(sellers_df, quantity_traded_df)
+
+        '''
         # find allocative efficiency of Huang mechanism
-        eff_Huang = allocative_efficiency_Huang(buyers_df,          sellers_df,
-                                                MCP_buyers_Huang,   MCP_sellers_Huang,
+        eff_Huang = allocative_efficiency_Huang(buyers_df, sellers_df,
+                                                MCP_buyers_Huang, MCP_sellers_Huang,
                                                 result)
-        eff_df.loc[index] = [eff_Huang]
 
-# append the averaged efficiency (constants: households and distributions) of mechanism to dataframe
-eff_df_mean.loc[n] = [eff_df.mean().values[0]]
+        eff_df.loc[index] = [eff_Huang]
+        '''
 
 # find averaged MCP of all iterations ran with same households and same distributions
 MCP_df_mean = calculate_mean_MCP(MCP_df, MCP_df_mean)
 
-# find profits and utilities of each mechanism
-utility_profits_df = calculate_profits_utility(buyers_df, sellers_df, utility_profits_df)
-
-#find quantity traded for each mechanism
-
-quantity_traded_df = calculate_market_liquidity(sellers_df, quantity_traded_df)
+# append the averaged efficiency (constants: households and distributions) of mechanism to dataframe
+#(NOT NEEDED FOR NOW) eff_df_mean.loc[n] = [eff_df.mean().values[0]]
 
 # save sellers_df and buyers_df to a pickle file
 
 print(sellers_df)
 print(buyers_df)
+print(MCP_df)
 print(MCP_df_mean)
 print(utility_profits_df)
 print(quantity_traded_df)
